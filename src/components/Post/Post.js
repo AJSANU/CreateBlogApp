904b888027bd2aa53982ab13ed7b220d9813befb
Abhishek Jha <cu.16bcs5007@gmail.com>
2022-12-13 03:40:16 +0530

    Fixing delete issue

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -25,6 +25,7 @@ export default function Post({ id, url, title, date, onDelete }) {
 
   function onSubmit() {
     onDelete(id);
+    setVisibility(false);
   }
 
   const handleLikeClick = () => {
